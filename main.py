37838b949bdaf82eab8c9f9131eaf0ca84259826
Jonatan Westholm <jdw@sentian.ai>
2019-07-04 16:55:15 +0200

    modularizing

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def get_ts(N, p=0, q=0):
 
     return model, model.generate_sample(N)
 
-def resample(data, N=None):
+def resample(data, N=None, std_horizontal=0.1):
     """Resamples data in uniform random time points,
     using linear interpolation
 
@@ -44,7 +44,7 @@ def resample(data, N=None):
 
     #times = np.random.random([N])*N
     times = np.linspace(0, N, N)
-    times = times + np.random.randn(N)*0
+    times = times + np.random.randn(N) * std_horizontal
     times = np.array(sorted(times))
     times = np.clip(times, 0, N-1e-6)
     return np.array([interpolate(data, t) for t in times]), times
@@ -71,40 +71,46 @@ def test_resampling():
     plt.scatter(times, irreg_data, color='r')
     plt.show()
 
+def fit(data, times, p=2):
+    fit_res = 10
+    width = p / fit_res
+    A, b, times_red = get_system_matrix(times, data, fit_res, width)
+    theta = np.linalg.lstsq(A, b, rcond=-1)
+    theta = theta[0]
+    print("Rec. using {0:d} piecewise, horizon {1:.3f}" \
+          .format(fit_res, fit_res * width))
+    return theta, A, b, times_red
+
 def test_fit():
     np.random.seed(0)
     p = 4
     N = 1000
-    model, data = get_ts(N, p=p)
-    times = np.linspace(0, N, N)
-    data, times = resample(data)
-
-    #print(model)
-    #exit(0)
-
-    fit_res = 10
-    width = p / fit_res
-    A, b, times_red = get_system_matrix(times, data, fit_res, width)
-
-    #print(A.shape)
-    #print(b.shape)
-
-    theta = np.linalg.lstsq(A, b, rcond=-1)
-    #print(theta)
-    #print(A)
-    theta = theta[0]
-    y_rec = np.dot(A, theta)
-
-
-    # Elaborate logging
+    model, orig_data = get_ts(N, p=p)
     print("AR({}), {} samples".format(p, N))
-    print("Rec. using {0:d} piecewise, horizon {1:.3f}" \
-          .format(fit_res, fit_res * width))
-    burn_in = len(data) - len(y_rec)
-    data = data[burn_in:]
-    times = times[burn_in:]
-    err = rmse(y_rec, data)
-    print("RMSE: {0:.3f}".format(err))
+    times = np.linspace(0, N, N)
+
+    for std_horizontal in np.linspace(0, 1):
+        data, times = resample(orig_data, std_horizontal=std_horizontal)
+
+        theta, A, b, times_red = fit(data, times, p)
+        y_rec = np.dot(A, theta)
+
+        err = rmse(b, y_rec)
+        print("RMSE: {0:.3f}".format(err))
+        burn_in = len(data) - len(y_rec)
+        residuals = b - y_rec
+
+        plt.cla()
+        plt.hist(residuals, bins=50)
+        plt.xlim([-10, 10])
+        plt.title("Horz std: {0:.2f}, RMSE: {1:.3f}".format(std_horizontal, err))
+        plt.draw()
+        plt.pause(0.5)
+    #plt.show()
+
+    # signal to noise ratio
+
+
 
     plt.plot(times, data, 'b')
     plt.scatter(times, data, color='b')
